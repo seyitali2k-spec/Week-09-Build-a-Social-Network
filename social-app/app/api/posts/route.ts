7fb2dd657d901af72c98d5a00482305cc5568725
Seyit ali Setirekli <seyitali2k@gmail.com>
2026-03-18 20:27:24 +0000

    fix api GET route

--- social-app/app/api/posts/route.ts
+++ social-app/app/api/posts/route.ts
@@ -1,10 +1,24 @@
 import { auth } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/prisma";
 
+export async function GET() {
+try {
+    const posts = await prisma.post.findMany({
+    include: { user: true },
+    orderBy: { createdAt: "desc" },
+});
+
+    return Response.json(posts);
+} catch (error) {
+    console.error("GET ERROR:", error);
+    return Response.json([], { status: 200 });
+}
+}
+
 export async function POST(req: Request) {
 const { userId } = await auth();
 
-  console.log("USER ID:", userId); // 👈 DEBUG
+console.log("USER ID:", userId);
 
 if (!userId) {
     return Response.json({ error: "Unauthorized" }, { status: 401 });
